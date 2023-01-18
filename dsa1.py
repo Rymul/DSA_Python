ad52b96376b3061dff5eeecac11a47f376802589
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-18 11:12:59 -0800

    complete anagrams

--- dsa1.py
+++ dsa1.py
@@ -110,3 +110,26 @@ def compress(s):
   return ''.join(res)
 
 print(compress('ccaaatsss'))
+
+
+# Anagrams
+# time complexity O(n+m) where n = len(s1) and m = len(s2)
+
+from collections import Counter
+
+def anagrams(s1, s2):
+  return Counter(s1) == Counter(s2)
+  # return char_count(s1) == char_count(s2)
+  
+  
+# def char_count(s):
+#   count = {}
+  
+#   for char in s:
+#     if char not in count:
+#       count[char] = 0
+#     count[char] += 1
+#   return count
+
+print(anagrams('restful', 'fluster'))
+print(anagrams('cats', 'tocs'))
