636b5688a96a02d1e671651be5eebd329c2e7570
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-18 10:42:04 -0800

    complete uncompress

--- dsa1.py
+++ dsa1.py
@@ -62,3 +62,24 @@ print(is_prime(2))
 print(is_prime(3))
 print(is_prime(4))
 print(is_prime(5))
+
+
+
+# Uncompress
+
+def uncompress(s):
+  i = 0
+  j = 0
+  res = []
+  nums = '0123456789'
+  while j < len(s):
+    if s[j] in nums:
+      j += 1
+    else:
+      num = int(s[i:j])
+      res.append(num * s[j])
+      j += 1
+      i = j
+  return ''.join(res)
+
+print(uncompress("2c3a1t"))
