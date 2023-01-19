04b6029df72ab1e1267329dabad0e90b74d2d99b
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-19 15:26:44 -0800

    complete Zipper List

--- dsa1.py
+++ dsa1.py
@@ -342,3 +342,29 @@ def recursive_reverse_list(head, previous = None):
   head.next = previous
   
   return recursive_reverse_list(next_node, head)
+
+
+# Zipper List
+
+def zipper_lists(head_1, head_2):
+  tail = head_1
+  current_1 = head_1.next
+  current_2 = head_2
+  count = 0
+  
+  while current_1 is not None and current_2 is not None:
+    if count % 2 == 0:
+      tail.next = current_2
+      current_2 = current_2.next
+    else:
+      tail.next = current_1
+      current_1 = current_1.next
+    tail = tail.next
+    count += 1
+    
+  if current_1 is not None:
+    tail.next = current_1
+  if current_2 is not None:
+    tail.next = current_2
+    
+  return head_1
