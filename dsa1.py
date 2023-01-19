8736f62097b8a48fe88e752d4ec46d34ab9864db
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-18 20:02:41 -0800

    complete most frequent char

--- dsa1.py
+++ dsa1.py
@@ -3,7 +3,7 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 
-def containsDuplicate( nums ) -> bool:
+def contains_duplicate( nums ) -> bool:
     count = {}
     for num in nums:
         if num not in count:
@@ -14,31 +14,31 @@ def containsDuplicate( nums ) -> bool:
         
     return False
 
-print(containsDuplicate([1,2,3,1]))
-print(containsDuplicate([1,2,3]))   
+print(contains_duplicate([1,2,3,1]))
+print(contains_duplicate([1,2,3]))   
 
 # better answer:
 
-def containsDuplicate2(nums) -> bool:
+def contains_duplicate2(nums) -> bool:
     unique = set(nums)
     if len(unique) < len(nums):
         return True
     return False
 
 
-print(containsDuplicate2([1,2,3,1]))
-print(containsDuplicate2([1,2,3]))
+print(contains_duplicate2([1,2,3,1]))
+print(contains_duplicate2([1,2,3]))
 
 
 
 # Max Value
 
-def max_value(nums):
-  max = float('-inf')
-  for num in nums:
-    if num > max:
-      max = num
-  return max
+def max_value(nums) -> int:
+    maxi = float('-inf')
+    for num in nums:
+        if num > maxi:
+            maxi = num
+    return maxi
     
 print(max_value([4, 7, 2, 8, 10, 9]))
 print(max_value([10, 5, 40, 40.3]))
@@ -50,13 +50,13 @@ print(max_value([-5, -2, -1, -11]))
 # Is Prime
 
 from math import sqrt, floor
-def is_prime(n):
-  if n < 2:
-    return False
-  for i in range(2, floor(sqrt(n)) +1):
-    if n % i == 0:
-      return False
-  return True
+def is_prime(n) -> int:
+    if n < 2:
+        return False
+    for i in range(2, floor(sqrt(n)) +1):
+        if n % i == 0:
+            return False
+    return True
 
 print(is_prime(2))
 print(is_prime(3))
@@ -67,20 +67,20 @@ print(is_prime(5))
 
 # Uncompress
 
-def uncompress(s):
-  i = 0
-  j = 0
-  res = []
-  nums = '0123456789'
-  while j < len(s):
-    if s[j] in nums:
-      j += 1
-    else:
-      num = int(s[i:j])
-      res.append(num * s[j])
-      j += 1
-      i = j
-  return ''.join(res)
+def uncompress(s) -> str:
+    i = 0
+    j = 0
+    res = []
+    nums = '0123456789'
+    while j < len(s):
+        if s[j] in nums:
+            j += 1
+        else:
+            num = int(s[i:j])
+            res.append(num * s[j])
+            j += 1
+            i = j
+    return ''.join(res)
 
 print(uncompress("2c3a1t"))
 
@@ -88,26 +88,26 @@ print(uncompress("2c3a1t"))
 
 # Compress
 
-def compress(s):
-  s += '!'
-  i = 0
-  j = 0
-  res = []
+def compress(s) -> str:
+    s += '!'
+    i = 0
+    j = 0
+    res = []
   
-  while j < len(s):
-    if s[j] == s[i]:
-      j += 1
-    else:
-      num = j-i
-      if num == 1:
-        res.append(s[i])
-      else:
-        res.append(str(num))
-        res.append(s[i])
-      i = j
+    while j < len(s):
+        if s[j] == s[i]:
+            j += 1
+        else:
+            num = j-i
+            if num == 1:
+                res.append(s[i])
+            else:
+                res.append(str(num))
+                res.append(s[i])
+            i = j
       
       
-  return ''.join(res)
+    return ''.join(res)
 
 print(compress('ccaaatsss'))
 
@@ -117,8 +117,8 @@ print(compress('ccaaatsss'))
 
 from collections import Counter
 
-def anagrams(s1, s2):
-  return Counter(s1) == Counter(s2)
+def anagrams(s1, s2) -> bool:
+    return Counter(s1) == Counter(s2)
   # return char_count(s1) == char_count(s2)
   
   
@@ -133,3 +133,16 @@ def anagrams(s1, s2):
 
 print(anagrams('restful', 'fluster'))
 print(anagrams('cats', 'tocs'))
+
+
+# Most Frequent Char
+
+def most_frequent_char(s) -> str:
+    count = Counter(s)
+    freq_char = None
+    for char in s:
+        if freq_char is None or count[char] > count[freq_char]:
+            freq_char = char
+    return freq_char
+
+print(most_frequent_char('bookeeper'))
