104adb0124d8fabc117c37a0fb9a83eb82e15b46
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-19 15:47:40 -0800

    complete Merge Lists

--- dsa1.py
+++ dsa1.py
@@ -385,3 +385,27 @@ def recursive_zipper_list(head_1, head_2):
     
     head_2.next = zipper_lists(next_1, next_2)
     return head_1
+
+
+# Merge Lists
+
+def merge_lists(head_1, head_2):
+  current_1 = head_1
+  current_2 = head_2
+  dummy_head = Node(None)
+  tail = dummy_head
+    
+  while current_1 is not None and current_2 is not None:
+    if current_1.val < current_2.val:
+      tail.next = current_1
+      current_1 = current_1.next
+    else:
+      tail.next = current_2
+      current_2 = current_2.next
+    tail = tail.next
+    
+  if current_1 is not None:
+    tail.next = current_1
+  if current_2 is not None:
+    tail.next = current_2
+  return dummy_head.next
