2f5b5337eb507e75e9ca77145656bd5cb4f3e3d8
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-20 09:19:30 -0800

    complete insert node

--- dsa1.py
+++ dsa1.py
@@ -504,3 +504,25 @@ def recursive_remove_node(head, target_val):
   
   head.next = recursive_remove_node(head.next, target_val)
   return head
+
+
+# Insert Node
+
+def insert_node(head, value, index):
+  if index == 0:
+    new_head = Node(value)
+    new_head.next = head
+    return new_head
+  
+  current = head
+  count = 0
+  
+  while current is not None:
+    if count == index - 1:
+      temp = current.next
+      current.next = Node(value)
+      current.next.next = temp
+    count += 1
+    current = current.next
+
+  return head
