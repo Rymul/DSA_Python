66dd2c75c6f9235c00d0fce948a190d0d90c1bf9
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-24 10:31:11 -0800

    complete recursive dfs binary tree

--- binary_trees.py
+++ binary_trees.py
@@ -1,17 +1,29 @@
 # DFS values
 
 def depth_first_values(root):
-  if root is None:
-    return []
-  stack = [root]
-  res = []
-  while stack:
-    current = stack.pop()
-    res.append(current.val)
-    
-    if current.right is not None:
-      stack.append(current.right)
-    if current.left is not None:
-      stack.append(current.left)
-      
-  return res
+    if root is None:
+        return []
+    stack = [root]
+    res = []
+    while stack:
+        current = stack.pop()
+        res.append(current.val)
+        
+        if current.right is not None:
+            stack.append(current.right)
+        if current.left is not None:
+            stack.append(current.left)
+        
+    return res
+
+# Recursive DFS values
+
+def recursive_depth_first_values(root):
+    if root is None:
+        return []
+    left_vals = depth_first_values(root.left)
+    right_vals = depth_first_values(root.right)
+    # return [root.val, *left_vals, *right_vals]
+    return [root.val] + left_vals + right_vals
+
+
