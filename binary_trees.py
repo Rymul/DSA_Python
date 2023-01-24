3ac5b7d7e9c8a58ef82f3c8d6bf0dd1430f89595
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-24 10:46:17 -0800

    complete bfs binary tree

--- binary_trees.py
+++ binary_trees.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 # DFS values
 
 def depth_first_values(root):
@@ -8,12 +14,10 @@ def depth_first_values(root):
     while stack:
         current = stack.pop()
         res.append(current.val)
-        
         if current.right is not None:
             stack.append(current.right)
         if current.left is not None:
             stack.append(current.left)
-        
     return res
 
 # Recursive DFS values
@@ -27,3 +31,20 @@ def recursive_depth_first_values(root):
     return [root.val] + left_vals + right_vals
 
 
+# BFS Binary Tree
+
+from collections import deque
+
+def breadth_first_values(root):
+    if root is None:
+        return []
+    queue = deque([ root ])
+    res = []
+    while queue:
+        current = queue.popleft()
+        res.append(current.val)
+        if current.left is not None:
+            queue.append(current.left)
+        if current.right is not None:
+            queue.append(current.right)
+    return res
