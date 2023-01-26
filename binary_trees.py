82190a6dc8433ea43ae31ef22eb1fe8b0570efbb
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-26 15:40:24 -0800

    complete tree height

--- binary_trees.py
+++ binary_trees.py
@@ -207,3 +207,13 @@ def tree_value_count(root, target):
         if current.right is not None:
             queue.append(current.right)
     return count
+
+# Tree Height
+
+def how_high(node):
+    if node is None:
+        return -1
+  
+    left_height = how_high(node.left)
+    right_height = how_high(node.right)
+    return 1 + max(left_height, right_height)
