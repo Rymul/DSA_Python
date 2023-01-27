2dccb315c94b1467a228bafa3e4e72e84ab99713
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-26 18:04:02 -0800

    complete all tree paths

--- binary_trees.py
+++ binary_trees.py
@@ -225,3 +225,19 @@ def bottom_right_value(root):
         if current.right is not None:
             queue.append(current.right)
     return current.val
+
+# All Tree Paths
+
+def all_tree_paths(root):
+    if root is None:
+        return []
+    if root.left is None and root.right is None:
+        return [[root.val]]
+    paths = []
+    left_paths = all_tree_paths(root.left)
+    for sub_path in left_paths:
+        paths.append([root.val, *sub_path])
+    right_paths = all_tree_paths(root.right)
+    for sub_path in right_paths:
+        paths.append([root.val, *sub_path])
+    return paths
