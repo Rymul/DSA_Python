2e0485bc9b8181e1d46f99e130fe51fabf5d9183
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-30 15:47:54 -0800

    complete recursive has path

--- graphs.py
+++ graphs.py
@@ -11,3 +11,14 @@ def has_path(graph, src, dst):
         for neighbor in graph[current]:
             stack.append(neighbor)
     return False
+
+
+# DFS recursive
+
+def recursive_has_path(graph, src, dst):
+    if src == dst:
+        return True
+    for neighbor in graph[src]:
+        if recursive_has_path(graph, neighbor, dst) is True:
+            return True
+    return False
