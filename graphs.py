fa4c0c3acb3b1593b214ba9c4b5a372b13a14eea
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-31 13:24:58 -0800

    complete semesters required

--- graphs.py
+++ graphs.py
@@ -238,3 +238,36 @@ def traverse_distance(graph, node, distance):
             max_length = attempt
     distance[node] = 1 + max_length
     return distance[node]
+
+
+# Semesters Required
+
+def semesters_required(num_courses, prereqs):
+    graph = create_graph(num_courses, prereqs)
+    distance = {}
+    for course in graph:
+        if len(graph[course]) == 0:
+            distance[course] = 1
+    for course in graph:
+        traverse_distance(graph, course, distance)
+    return max(distance.values())
+
+def travel_distance(graph, node, distance):
+    if node in distance:
+        return distance[node]
+    max_distance = 0
+    for neighbor in graph[node]:
+        neighbor_distance = travel_distance(graph, neighbor, distance)
+        if neighbor_distance > max_distance:
+            max_distance = neighbor_distance
+    distance[node] = 1 + max_distance
+    return distance[node]
+
+def create_graph(num_courses, prereqs):
+    graph = {}
+    for course in range(num_courses):
+        graph[course] = []
+    for prereq in prereqs:
+        a, b = prereq
+        graph[a].append(b)
+    return graph
