05856ffdf6476d3b03db734dc1259816dab5a9d6
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-31 12:33:50 -0800

    complete closest carrot

--- graphs.py
+++ graphs.py
@@ -193,3 +193,25 @@ def island_size(grid, r, c, visited):
     size += island_size(grid, r, c -1, visited)
     size += island_size(grid, r, c +1, visited)
     return size
+
+# Closest Carrot
+
+def closest_carrot(grid, starting_row, starting_col):
+    visited = set([(starting_row, starting_col)])
+    queue = deque([ (starting_row, starting_col, 0) ])
+    while queue:
+        row, col, distance = queue.popleft()
+        if grid[row][col] == 'C':
+            return distance
+        deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for delta in deltas:
+            delta_row, delta_col = delta
+            neighbor_row = row + delta_row
+            neighbor_col = col + delta_col
+            row_inbounds = 0 <= neighbor_row < len(grid)
+            col_inbounds = 0 <= neighbor_col < len(grid[0])
+            pos = (neighbor_row, neighbor_col)
+            if row_inbounds and col_inbounds and grid[neighbor_row][neighbor_col] != 'X' and pos not in visited:
+                queue.append((neighbor_row, neighbor_col, distance +1))
+                visited.add(pos)
+    return -1
