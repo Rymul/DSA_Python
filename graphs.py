2c77eea20077165334ef7be0762ead0fa87eb548
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-31 12:46:05 -0800

    complete longest path

--- graphs.py
+++ graphs.py
@@ -215,3 +215,26 @@ def closest_carrot(grid, starting_row, starting_col):
                 queue.append((neighbor_row, neighbor_col, distance +1))
                 visited.add(pos)
     return -1
+
+# Longest Path
+
+def longest_path(graph):
+    distance = {}
+    for node in graph:
+        if len(graph[node]) == 0:
+            distance[node] = 0
+    for node in graph:
+        traverse_distance(graph, node, distance)
+    return max(distance.values())
+
+
+def traverse_distance(graph, node, distance):
+    if node in distance:
+        return distance[node]
+    max_length = 0
+    for neighbor in graph[node]:
+        attempt = traverse_distance(graph, neighbor, distance)
+        if attempt > max_length:
+            max_length = attempt
+    distance[node] = 1 + max_length
+    return distance[node]
