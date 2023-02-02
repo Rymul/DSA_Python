bf17ca52fab873b2ab1073c4f136c32d27a82182
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-02 13:31:41 -0800

    complete max palindrome subsequence with memoization

--- dynamic_programing.py
+++ dynamic_programing.py
@@ -349,3 +349,31 @@ print(array_stepper([
   11, 10, 9, 8, 7, 6,
   5, 3, 2, 1, 0, 0, 0
 ])) # False
+
+
+# Max Palindrome Subsequence
+
+def max_palin_subsequence(string):
+    """return the length of the longest palindrome subsequence of the string"""
+    return _max_palin_subsequence(string, 0, len(string)-1, {})
+
+
+def _max_palin_subsequence(string, i, j, memo):
+    key = (i, j)
+    if key in memo:
+        return memo[key]
+    if i == j:
+        return 1
+    if i > j:
+        return 0
+    if string[i] == string[j]:
+        memo[key] = 2 + _max_palin_subsequence(string, i+1, j-1, memo)
+    else:
+        memo[key] = max(
+        _max_palin_subsequence(string, i+1, j, memo),
+        _max_palin_subsequence(string, i, j-1, memo)
+        )
+    return memo[key]
+
+print(max_palin_subsequence("xyzaxxzy")) # 6
+print(max_palin_subsequence("enamelpinportlandtildecoldpressedironyflannelsemioticsedisonbulbfashionaxe")) # 31
