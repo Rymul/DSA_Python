ee40f3bcdcbe634523a0e411c989cf3125d87c6e
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-07 09:54:34 -0800

    complete properly formatted brackets

--- stacks.py
+++ stacks.py
@@ -1,3 +1,5 @@
+# Paird Parentheses
+
 def paired_parentheses(string) -> bool:
     """Returns a boolean indicating if the string has well formated parentheses"""
     count = 0
@@ -12,3 +14,28 @@ def paired_parentheses(string) -> bool:
 
 print(paired_parentheses("(david)((abby))")) # True
 print(paired_parentheses(")(")) # False
+
+
+# Properly Formatted Brackets
+
+def properly_formatted_brackets(string):
+    """Returns a boolean indicating if the string has properly formated brackets"""
+    stack = []
+
+    symbols = {
+        '(' : ')',
+        '[' : ']',
+        '{' : '}'
+    }
+    for char in string:
+        if char in symbols:
+            stack.append(symbols[char])
+        else:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
+
+print(properly_formatted_brackets('(){}[](())')) # True
+print(properly_formatted_brackets('[]{}(}[]')) # False
