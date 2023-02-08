8de204bdabb4ca0e688a1f327b3bdd40907199fa
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-08 14:09:11 -0800

    complete recursive linked list values

--- linked-list-practice-1.py
+++ linked-list-practice-1.py
@@ -11,6 +11,17 @@ def linked_list_values(head):
         current = current.next
     return res
 
+def recursive_linked_list_values(head):
+    res = []
+    _recursive_linked_list_values(head, res)
+    return res
+
+def _recursive_linked_list_values(head, res):
+    if head is None:
+        return []
+    res.append(head.val)
+    _recursive_linked_list_values(head.next, res)
+
 a = Node("a")
 b = Node("b")
 c = Node("c")
@@ -21,4 +32,5 @@ b.next = c
 c.next = d
 
 print(linked_list_values(a))
+print(recursive_linked_list_values(a))
     
