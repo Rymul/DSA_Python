97e1b93ccf0726ba92d4809a6ddbcddccea5013b
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-08 12:44:38 -0800

    complete compress

--- string_and_list_practice.py
+++ string_and_list_practice.py
@@ -15,3 +15,26 @@ def uncompress(s):
     return ''.join(res)
 
 print(uncompress("3n12e2z"))
+
+
+def compress(s):
+    i = 0
+    j = 0
+    s += '!'
+    res = []
+
+    while j < len(s):
+        if s[j] == s[i]:
+            j += 1
+        else:
+            num = j - i
+            if num == 1:
+                res.append(s[i])
+            else:
+                res.append(str(num))
+                res.append(s[i])
+            i = j
+    return ''.join(res)
+
+print(compress('ccaaatsss'))
+        
