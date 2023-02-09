84f629c953b191f892951c577d4536a0f1eae409
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-08 16:28:47 -0800

    complete zipper list

--- linked-list-practice-1.py
+++ linked-list-practice-1.py
@@ -142,3 +142,62 @@ def recursive_reverse_list(head, prev = None):
     return recursive_reverse_list(temp, head)
 
 print(recursive_reverse_list(a))
+
+def zipper_lists(head_1, head_2):
+    tail = head_1
+    cur_1 = head_1.next
+    cur_2 = head_2
+    count = 0
+    while cur_1 is not None and cur_2 is not None:
+        if count % 2 == 0:
+            tail.next = cur_2
+            cur_2 = cur_2.next
+        else:
+            tail.next = cur_1
+            cur_1 = cur_1.next
+        tail = tail.next
+        count += 1
+    if cur_1 is not None:
+        tail.next = cur_1
+    if cur_2 is not None:
+        tail.next = cur_2
+    return linked_list_values(head_1)
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+a.next = b
+b.next = c
+# a -> b -> c
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+# x -> y -> z
+
+print(zipper_lists(a, x))
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+# a -> b -> c -> d -> e -> f
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+# x -> y -> z
+
+print(zipper_lists(a, x))
+# a -> x -> b -> y -> c -> z -> d -> e -> f
