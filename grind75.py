63cf02e91abb87202cc6efd3ca850d76a3f21edb
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-21 15:41:07 -0800

    complete valid parentheses

--- grind75.py
+++ grind75.py
@@ -10,3 +10,27 @@ def twoSum(nums, target):
 n = [2,7,11,15]
 t = 9
 print(twoSum(n, t))
+
+
+#20. Valid Parentheses
+
+def isValidParens(s):
+    parens = {
+        "{" : "}",
+        "[" : "]",
+        "(" : ")"
+    }
+    stack = []
+    for char in s:
+        if char in parens:
+            stack.append(char)
+        elif len(stack) == 0 or parens[stack[-1]] != char:
+            return False
+        else:
+            stack.pop()
+    return len(stack) == 0
+
+s1 = "()[]{}"
+s2 = "(]"
+print(isValidParens(s1))
+print(isValidParens(s2))
