84d595fa953e70ca3514180772fa4a2d3ce4cb07
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-22 12:55:41 -0800

    complete merge two sorted linked lists

--- grind75.py
+++ grind75.py
@@ -34,3 +34,27 @@ s1 = "()[]{}"
 s2 = "(]"
 print(isValidParens(s1))
 print(isValidParens(s2))
+
+
+#21 Merge 2 Sorted Lists
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(l1, l2):
+    dummy = ListNode()
+    current = dummy
+
+    while l1 and l2:
+        if l1.val <= l2.val:
+            current.next = l1
+            l1 = l1.next
+        else:
+            current.next = l2
+            l2 = l2.next
+        current = current.next
+    current.next = l1 or l2
+    return dummy.next
+
