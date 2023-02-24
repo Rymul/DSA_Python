02e7d7010980b337293ee9b526955fb724ce99f3
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-24 14:55:38 -0800

    complete lowest common ancestor

--- grind75.py
+++ grind75.py
@@ -161,3 +161,13 @@ sc1 = 1
 c = 2
 
 print(floodFill(i, sr1, sc1, c))
+
+
+#235. Lowest Common Ancestor of a Binary Search Tree
+def lowestCommonAncestor(self, root, p, q):
+    if root.val > p.val and root.val > q.val:
+        return self.lowestCommonAncestor(root.left, p, q)
+    elif root.val < p.val and root.val < q.val:
+        return self.lowestCommonAncestor(root.right, p, q)
+    else:
+        return root
