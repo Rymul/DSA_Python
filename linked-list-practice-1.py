4e4489fdf619182c03fc4351f8ed75015da369a9
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-15 22:26:29 -0700

    complete first bad version

--- linked-list-practice-1.py
+++ linked-list-practice-1.py
@@ -361,3 +361,20 @@ def hasCycle(head):
 print(hasCycle(a)) # False
 f.next = a
 print(hasCycle(a)) # True
+
+
+# 278. First Bad Version
+# function first bad version is given
+
+def firstBadVersion(n):
+    left = 1
+    right = n
+    while left < right:
+        mid = left + (right - left)//2
+        if isBadversion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+
