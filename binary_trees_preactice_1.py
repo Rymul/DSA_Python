4ac9cb92b73bf6bbef87dc2a5f806807aa817a95
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-21 20:19:38 -0700

    complete tree includes

--- binary_trees_preactice_1.py
+++ binary_trees_preactice_1.py
@@ -90,3 +90,39 @@ c.right = f
 
 print(tree_sum(a)) # -> 21
 print(recursive_tree_sum(a))
+
+def tree_includes(root, target):
+    if root.val == target:
+        return True
+    queue = deque([root])
+    while queue:
+        current = queue.popleft()
+        if current.val == target:
+            return True
+        if current.left is not None:
+            queue.append(current.left)
+        if current.right is not None:
+            queue.append(current.right)
+    return False
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(tree_includes(a, "e")) # -> True
+print(tree_includes(a, "z")) # -> False
