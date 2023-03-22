118c69f5e0a50eae4187431eaa110a5f6760419c
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-21 20:11:58 -0700

    complete tree sum

--- binary_trees_preactice_1.py
+++ binary_trees_preactice_1.py
@@ -49,3 +49,37 @@ def breadth_first_values(root):
     return res
 
 print(breadth_first_values(a))
+
+def tree_sum(root):
+    queue = deque([root])
+    total_sum = 0
+    while queue:
+        current = queue.popleft()
+        total_sum += current.val
+        if current.left is not None:
+            queue.append(current.left)
+        if current.right is not None:
+            queue.append(current.right)
+    return total_sum
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+print(tree_sum(a)) # -> 21
