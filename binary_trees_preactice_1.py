4b19af945380d6d9340f322b141c75c5d1d3c8e4
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-22 21:51:23 -0700

    complete max path sum

--- binary_trees_preactice_1.py
+++ binary_trees_preactice_1.py
@@ -178,3 +178,34 @@ c.right = f
 
 print(tree_min_value(a)) # -> -2
 print(recursive_tree_min_value(a))
+
+
+def max_path_sum(root):
+    if root is None:
+        return float('-inf')
+    if root.left is None and root.right is None:
+        return root.val
+    max_left = max_path_sum(root.left)
+    max_right = max_path_sum(root.right)
+    return root.val + max(max_left, max_right)
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+print(max_path_sum(a)) # -> 18
