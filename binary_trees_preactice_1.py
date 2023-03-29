82de6c7903e47ec93aa27e85bc7964be9918aba4
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-28 19:57:37 -0700

    complete all tree paths

--- binary_trees_preactice_1.py
+++ binary_trees_preactice_1.py
@@ -353,3 +353,45 @@ c.right = f
 # 4   -2     1
 
 print(bottom_right_value(a)) # -> 1
+
+
+def all_tree_paths(root):
+    if root is None:
+        return []
+    if root.left is None and root.right is None:
+        return [[root.val]]
+    paths = []
+    left_path = all_tree_paths(root.left)
+    for sub_path in left_path:
+        paths.append([root.val, *sub_path])
+    right_path = all_tree_paths(root.right)
+    for sub_path in right_path:
+        paths.append([root.val, *sub_path])
+    return paths
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(all_tree_paths(a)) # ->
+# [ 
+#   [ 'a', 'b', 'd' ], 
+#   [ 'a', 'b', 'e' ], 
+#   [ 'a', 'c', 'f' ] 
+# ] 
