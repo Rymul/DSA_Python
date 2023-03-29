08e27957c02ad7c70f835e84b344dd4173baa913
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-28 18:57:50 -0700

    complete how high

--- binary_trees_preactice_1.py
+++ binary_trees_preactice_1.py
@@ -293,3 +293,32 @@ c.right = f
 
 print(tree_value_count(a,  6)) # -> 3
 print(recursive_tree_value_count(a, 6))
+
+
+def how_high(root):
+    if root is None:
+        return -1
+    left = how_high(root.left)
+    right = how_high(root.right)
+    return 1 + max(left, right)
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(how_high(a)) # -> 2
