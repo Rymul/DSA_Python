4dbf96a79745ee8e10358322c48faf8cf2d0ca7c
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-29 21:12:15 -0700

    complete iterative tree levels

--- binary_trees_preactice_1.py
+++ binary_trees_preactice_1.py
@@ -411,6 +411,24 @@ def fill_levels(root, levels, level_num):
     fill_levels(root.left, levels, level_num+1)
     fill_levels(root.right, levels, level_num+1)
 
+def iterative_tree_levels(root):
+    if root is None:
+        return []
+    levels = []
+    queue = deque( [(root, 0)] )
+    while queue:
+        current, level_num = queue.popleft()
+        if len(levels) == level_num:
+            levels.append([current.val])
+        else:
+            levels[level_num].append(current.val)
+        if current.left is not None:
+            queue.append((current.left, level_num+1))
+        if current.right is not None:
+            queue.append((current.right, level_num+1))
+    return levels
+
+
 a = Node('a')
 b = Node('b')
 c = Node('c')
@@ -430,7 +448,8 @@ c.right = f
 #  / \     \
 # d   e     f
 
-print(tree_levels(a)) # ->
+print(tree_levels(a))
+print(iterative_tree_levels(a)) # ->
 # [
 #   ['a'],
 #   ['b', 'c'],
