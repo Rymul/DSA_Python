11246e49faf9a1fd23d3c0e407b584415fa810fb
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-29 21:39:28 -0700

    complete leaf list

--- binary_trees_preactice_1.py
+++ binary_trees_preactice_1.py
@@ -485,3 +485,37 @@ c.right = f
 # 4   -2     1
 
 print(level_averages(a)) # -> [ 3, 7.5, 1 ] 
+
+def leaf_list(root):
+    leaves = []
+    _leaf_list(root, leaves)
+    return leaves
+
+def _leaf_list(root, leaves):
+    if root is None:
+        return
+    if root.left is None and root.right is None:
+        leaves.append(root.val)
+    _leaf_list(root.left, leaves)
+    _leaf_list(root.right, leaves)
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(leaf_list(a)) # -> [ 'd', 'e', 'f' ]
