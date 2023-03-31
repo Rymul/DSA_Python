8f1142f574432cc5b6489b0576f6d284fc52e591
Ryan Mullen <ryanfmullen@gmail.com>
2023-03-30 19:27:26 -0700

    complete has path

---
+++ graphs_practice_1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def has_path(graph, src, dst):
+    queue = deque([src])
+    while queue:
+        current = queue.popleft()
+        if current == dst:
+            return True
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+    return False
+
+
+
+graph_1 = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+graph_2 = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+
+print(has_path(graph_1, 'f', 'k')) # True
+print(has_path(graph_2, 'f', 'j')) # False
