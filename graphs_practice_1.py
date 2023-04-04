d2b32b51b10d490bad9d748fc128e9f982fc42ae
Ryan Mullen <ryanfmullen@gmail.com>
2023-04-03 20:15:32 -0700

    complete connected component count

--- graphs_practice_1.py
+++ graphs_practice_1.py
@@ -80,3 +80,30 @@ edges = [
 ]
 
 print(undirected_path(edges, 'j', 'm')) # -> True
+
+
+def connected_components_count(graph):
+    visited = set()
+    count = 0
+    for node in graph:
+        if explore(graph, node, visited) is True:
+            count += 1
+    return count
+  
+def explore(graph, current, visited):
+    if current in visited:
+        return False
+    visited.add(current)
+    for neighbor in graph[current]:
+        explore(graph, neighbor, visited)
+    return True
+
+print(connected_components_count({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+})) # -> 2
