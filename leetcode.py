19d9caec8538e3f38f5999b6589df71f1015f645
Ryan Mullen <ryanfmullen@gmail.com>
2023-04-19 21:23:20 -0700

    complete pascals triangle 2

--- leetcode.py
+++ leetcode.py
@@ -13,3 +13,18 @@ def pascalsTriangle(numRows) -> list:
 
 print(pascalsTriangle(5))
 print(pascalsTriangle(1))
+
+# 119. Pascal's Triangle II
+
+def pascalsTriangle2(rowIndex)-> list:
+    prev=[1]
+    for i in range(1,1+rowIndex):
+        curr=[1]
+        for j in range(1,i):
+            curr.append(prev[j]+prev[j-1])
+        curr.append(1)
+        prev=curr
+    return prev
+
+print(pascalsTriangle2(3))
+print(pascalsTriangle2(1))
