90d1e3fbd2d6f8300a71dcf5788741854b6a3d77
Ryan Mullen <ryanfmullen@gmail.com>
2023-07-13 21:23:18 -0700

    complete leaf-similar trees

--- leetcode.py
+++ leetcode.py
@@ -384,3 +384,13 @@ def tree2str(root) -> str:
     if root.right:
         s += '({})'.format(tree2str(root.right))
     return s
+
+# 872. Leaf-Similar Trees
+
+def leafSimilar(root1, root2) -> bool:
+    def dfs(root):
+        if not root: return []
+        if not root.left and not root.right:
+            return [root.val]
+        return dfs(root.left) + dfs(root.right)
+    return dfs(root1) == dfs(root2)
